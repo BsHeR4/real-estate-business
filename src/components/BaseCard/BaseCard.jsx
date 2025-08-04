7196c6e859656a53b11db3e99807f2b528fe7ca4
M-Karakit <mohamad.karakit98@gmail.com>
2025-08-04 23:24:21 +0300

    Create BaseCard component with customizable padding

---
+++ src/components/BaseCard/BaseCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './BaseCard.css'
+
+const BaseCard = ({children, padding}) => {
+    const paddingClass = padding === 'large' ? 'mk-large-padding' : 'mk-medium-padding'
+    return (
+        <div className={`mk-card ${paddingClass}`}>
+            {children}
+        </div>
+    )
+}
+export default BaseCard
