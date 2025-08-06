7c5c1a28d36c8af3c1e76cafd7c3ed146564dc32
zaher-sankari <zaher.lp1@gmail.com>
2025-08-06 20:07:46 +0300

    Fix: fixed banner height and font-size on smaller screens

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import Banner from './Components/Banner/Banner.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Banner />
       <App />
     </BrowserRouter>
   </StrictMode>,
