e885992e2fe589d511bae134a7abdb2e2db5b729
zaher-sankari <zaher.lp1@gmail.com>
2025-08-13 22:57:42 +0300

    need to be fixed

--- src/Components/Villa/Villa.jsx
+++ src/Components/Villa/Villa.jsx
@@ -10,6 +10,8 @@ import BaseCard from './../BaseCard/BaseCard.jsx'
 
 const VillaPage = () => {
     return (
+        //TODO: redesign header section coz it sucks :|
+        //TODO: Fix image positioning on smaller screens
         <Section>
             <div className="zs-villa-wrapper">
                 <div className="zs-villa-header">
@@ -25,6 +27,9 @@ const VillaPage = () => {
                         <p className='zs-villa-price'>$1,250,000</p>
                     </div>
                 </div>
+                <BaseCard className='zs-img-mansion'>
+                <img className='' src="assets\imgs\Property\mansion.png" alt="Mansion" />
+                </BaseCard>
             </div>
         </Section>
     );
--- src/components/BaseCard/BaseCard.jsx
+++ src/components/BaseCard/BaseCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './BaseCard.css'
 
-const BaseCard = ({children, padding}) => {
+const BaseCard = ({children, padding, className}) => {
     const paddingClass = padding === 'large' ? 'mk-large-padding' : 'mk-medium-padding'
     return (
-        <div className={`mk-card ${paddingClass}`}>
+        <div className={`mk-card ${className} ${paddingClass}`}>
             {children}
         </div>
     )
