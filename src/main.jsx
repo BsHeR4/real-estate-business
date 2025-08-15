11ab7b3518bcbb332f206f597bbe5160def604da
zaher-sankari <zaher.lp1@gmail.com>
2025-08-15 16:52:32 +0300

    fixed responsive &  section is done.

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar.jsx'
-import Villa from './Components/Villa/Villa.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
       <App />
-      <Villa />
     </BrowserRouter>
   </StrictMode>,
 )
