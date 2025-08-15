71db2a3bc26e8c61c160cd67d41515fbf1f5eb77
M-Karakit <mohamad.karakit98@gmail.com>
2025-08-15 22:07:28 +0300

    Create Comprehensive Section

--- src/components/BaseCard/BaseCard.jsx
+++ src/components/BaseCard/BaseCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './BaseCard.css'
 
-const BaseCard = ({children, padding}) => {
-    const paddingClass = padding === 'large' ? 'mk-large-padding' : 'mk-medium-padding'
+const BaseCard = ({children, padding, className=''}) => {
+    const paddingClass =
+            padding === 'large' ? 'mk-large-padding' :
+            padding === 'small' ? 'mk-small-padding' :
+                                  'mk-medium-padding';
     return (
-        <div className={`mk-card ${paddingClass} mk-small-padding` }>
+        <div className={`mk-card ${paddingClass} ${className}`}>
             {children}
         </div>
     )
