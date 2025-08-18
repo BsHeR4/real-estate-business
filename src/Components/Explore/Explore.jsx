a6aee90565e2f86d5686f93c45c9bad5237c23eb
zaher-sankari <zaher.lp1@gmail.com>
2025-08-18 21:29:03 +0300

    made the images dynamic by moving it to a seperate file

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -1,24 +1,26 @@
 import SectionHeader from './../sectionheader/SectionHeader'
 import Section from './../Section/Section'
+import ExploreImages from './ExploreImages'
 import './Explore.css'
 export default function Explore() {
     return (
         <>
             <Section>
-                {/* TODO: make the image dynamic by moving it to another file */}
                 <div className="zs-explore-wrapper">
-                    <img src="\assets\imgs\explore-1.png" alt="" className='zs-explore-img zs-explore-img1' />
-                    <img src="\assets\imgs\explore-2.png" alt="" className='zs-explore-img zs-explore-img2' />
-                    <img src="\assets\imgs\explore-3.png" alt="" className='zs-explore-img zs-explore-img3' />
-                    <img src="\assets\imgs\explore-4.png" alt="" className='zs-explore-img zs-explore-img4' />
-                    <img src="\assets\imgs\explore-5.png" alt="" className='zs-explore-img zs-explore-img5' />
+                    {ExploreImages.map((image, index) => (
+                        <img
+                            key={index}
+                            className={image.className}
+                            src={image.src}
+                            alt={image.alt}
+                        />
+                    ))}
                     <SectionHeader
                         className="zs-explore-info"
                         title="Explore Estatein's World"
                         subtitle='Step inside the world of Estatein, where professionalism meets warmth, and expertise meets passion. Our gallery offers a glimpse into our team and workspaces, inviting you to get to know us better.'
                     >
                     </SectionHeader>
-                    <img src="\assets\imgs\explore-6.png" alt="" className='zs-explore-img zs-explore-img6' />
                 </div>
             </Section>
         </>
---
+++ src/Components/Explore/ExploreImages.jsx
@@ -0,0 +1,33 @@
+const ExploreImages = [
+    {
+        src: "assets/imgs/explore-1.png",
+        className: "zs-explore-img zs-explore-img1",
+        alt: "Office workspace"
+    },
+    {
+        src: "assets/imgs/explore-2.png",
+        className: "zs-explore-img zs-explore-img2",
+        alt: "Team"
+    },
+    {
+        src: "assets/imgs/explore-3.png",
+        className: "zs-explore-img zs-explore-img3",
+        alt: "Meeting"
+    },
+    {
+        src: "assets/imgs/explore-4.png",
+        className: "zs-explore-img zs-explore-img4",
+        alt: "Team smiling"
+    },
+    {
+        src: "assets/imgs/explore-5.png",
+        className: "zs-explore-img zs-explore-img5",
+        alt: "Team crew"
+    },
+    {
+        src: "assets/imgs/explore-6.png",
+        className: "zs-explore-img zs-explore-img6",
+        alt: "Business handshake"
+    }
+];
+export default ExploreImages
