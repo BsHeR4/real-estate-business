3b781b10ecbd02c7b2708d7b0cb9a236023f3edb
zaher-sankari <zaher.lp1@gmail.com>
2025-08-18 20:54:55 +0300

    Fixed classes names + responsive - todo: make the images dynamic using a seperate file

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -4,22 +4,23 @@ import './Explore.css'
 export default function Explore() {
     return (
         <>
-        <Section>
-            <div className="zs-explore-wrapper">
-                <img src="\assets\imgs\explore-1.png" alt="" />
-                <img src="\assets\imgs\explore-2.png" alt="" />
-                <img src="\assets\imgs\explore-3.png" alt="" />
-                <img src="\assets\imgs\explore-4.png" alt="" />
-                <img src="\assets\imgs\explore-5.png" alt="" />
-                <SectionHeader
-                className="zs-explore-info"
-                title="Explore Estatein's World"
-                subtitle='Step inside the world of Estatein, where professionalism meets warmth, and expertise meets passion. Our gallery offers a glimpse into our team and workspaces, inviting you to get to know us better.'
-                >
-                </SectionHeader>
-                <img src="\assets\imgs\explore-6.png" alt="" />
-            </div>
-        </Section>
+            <Section>
+                {/* TODO: make the image dynamic by moving it to another file */}
+                <div className="zs-explore-wrapper">
+                    <img src="\assets\imgs\explore-1.png" alt="" className='zs-explore-img zs-explore-img1' />
+                    <img src="\assets\imgs\explore-2.png" alt="" className='zs-explore-img zs-explore-img2' />
+                    <img src="\assets\imgs\explore-3.png" alt="" className='zs-explore-img zs-explore-img3' />
+                    <img src="\assets\imgs\explore-4.png" alt="" className='zs-explore-img zs-explore-img4' />
+                    <img src="\assets\imgs\explore-5.png" alt="" className='zs-explore-img zs-explore-img5' />
+                    <SectionHeader
+                        className="zs-explore-info"
+                        title="Explore Estatein's World"
+                        subtitle='Step inside the world of Estatein, where professionalism meets warmth, and expertise meets passion. Our gallery offers a glimpse into our team and workspaces, inviting you to get to know us better.'
+                    >
+                    </SectionHeader>
+                    <img src="\assets\imgs\explore-6.png" alt="" className='zs-explore-img zs-explore-img6' />
+                </div>
+            </Section>
         </>
     )
 }
