58b20231bd66597794621311b2333edd25af8609
BsHeR4 <bshermahayni@gmail.com>
2025-08-20 21:54:31 +0300

    change linkedIn icon

--- src/Components/AllRightsFooter/SocialData.jsx
+++ src/Components/AllRightsFooter/SocialData.jsx
@@ -1,8 +1,9 @@
-import { FaFacebookF, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa"
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa"
+import { RxLinkedinLogo } from "react-icons/rx";
 
 const SocialData = [
     { content: <FaFacebookF />, url: "facebook.com" },
-    { content: <FaLinkedinIn />, url: "linkedin.com" },
+    { content: <RxLinkedinLogo />, url: "linkedin.com" },
     { content: <FaTwitter />, url: "twitter.com" },
     { content: <FaYoutube />, url: "youtube.com" }
 ]
