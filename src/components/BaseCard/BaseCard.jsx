a02cd6e566f371d04ba8518ddaed38a73f8e9986
BsHeR4 <bshermahayni@gmail.com>
2025-08-27 23:20:48 +0300

    Updates

---
+++ src/components/BaseCard/BaseCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './BaseCard.css'
+
+/**
+ * BaseCard Component
+ *
+ * A reusable base card component that provide padding options.
+ *
+ * Props:
+ * - children (node): Content to be displayed inside the card.
+ * - padding (string): Padding size ('large' or 'medium').
+ *
+ * Usage:
+ * <BaseCard padding='large' -> 'for desktop padding 50px, 40px on laptops and 30px for mobiles and taps'  |  padding='medium' -> 'for desktop padding 40px, 30px on laptops and 24px for mobiles and taps'>
+ *   <h2>Card Title</h2>
+ *   <p>Card content</p>
+ * </BaseCard>
+ *
+ * Dependencies:
+ * - BaseCard.css: Styling module
+ */
+const BaseCard = ({ children, padding, className = '' }) => {
+    const paddingClass =
+        padding === 'large' ? 'mk-large-padding' :
+            padding === 'small' ? 'mk-small-padding' :
+                'mk-medium-padding';
+    return (
+        <div className={`mk-card ${paddingClass} ${className}`}>
+            {children}
+        </div>
+    )
+}
+export default BaseCard
